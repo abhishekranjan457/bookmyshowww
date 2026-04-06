1e2b1e48d9ad77438afa88cde8c7ec08832843e0
abhishek62004 <abhishekranjan170797@gmail.com>
2026-04-07 04:31:26 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("BookMyShow backend running");
+});
+
 app.use("/api", bookingRoutes);
 
 const PORT = process.env.PORT || 8080;
