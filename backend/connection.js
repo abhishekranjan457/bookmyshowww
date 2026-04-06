a2d0aed0fa48550df762fdf0b26fedd648bddd51
abhishek62004 <abhishekranjan170797@gmail.com>
2026-04-07 03:33:06 +0530

    add mongodb atlas

--- backend/connection.js
+++ backend/connection.js
@@ -1,8 +1,9 @@
-
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect("mongodb://127.0.0.1:27017/bookmyshow");
+  return mongoose.connect(
+    "mongodb+srv://abhishek:Abhi@123@cluster0.hl55a9a.mongodb.net/bookmyshow",
+  );
 };
 
 module.exports = connect;
