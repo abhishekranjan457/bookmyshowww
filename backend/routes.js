3ca380a724b42b1889f2c10bd19c8646befe73fd
abhishek62004 <abhishekranjan170797@gmail.com>
2026-04-07 05:07:11 +0530

    fix last booking DB fetch

--- backend/routes.js
+++ backend/routes.js
@@ -2,12 +2,9 @@ const express = require("express");
 const router = express.Router();
 const Booking = require("./Schema");
 
-let lastBooking = null;
-
 router.post("/booking", async (req, res) => {
   try {
     const booking = await Booking.create(req.body);
-    lastBooking = booking;
     res.status(200).send(booking);
   } catch (e) {
     res.status(500).send(e.message);
@@ -15,10 +12,17 @@ router.post("/booking", async (req, res) => {
 });
 
 router.get("/booking", async (req, res) => {
-  if (!lastBooking) {
-    return res.send({ message: "no previous booking found" });
+  try {
+    const lastBooking = await Booking.findOne().sort({ _id: -1 });
+
+    if (!lastBooking) {
+      return res.send({ message: "no previous booking found" });
+    }
+
+    res.send(lastBooking);
+  } catch (e) {
+    res.status(500).send(e.message);
   }
-  res.send(lastBooking);
 });
 
 module.exports = router;
